3fa14f05df7b53c7fdf0135bccfbe90e45957b77
Marcus-Holanda777 <62726510+Marcus-Holanda777@users.noreply.github.com>
2021-07-01 15:52:42 -0300

    atualizar

--- backup.py
+++ backup.py
@@ -2,6 +2,7 @@ import os
 from subprocess import Popen
 from datetime import datetime
 import zipfile
+import shutil
 
 
 class Mysqlbk:
@@ -22,6 +23,12 @@ class Mysqlbk:
             print(e)
         else:
             os.unlink(saida)
+    
+    def copiarBk(self, origem: str) -> None:
+        destino = '\\\\pmenos\\setores\\ProjetodePrevencaodePerdas\\09. Provisório\Marcus\\bk_mysql'
+        arq = origem.split('\\')[-1][:-3] + '.zip'
+
+        shutil.copy(origem[:-3] + 'zip', os.path.join(destino, arq))
 
 
     def fazerBackup(self, pastasaida: str) -> None:
@@ -33,9 +40,12 @@ class Mysqlbk:
         Popen(comando, cwd=self.raiz, shell=True).wait()
 
         self.compactarBk(saida)
+        self.copiarBk(saida)
 
 
 my = Mysqlbk(datetime.now(),
              'C:\Program Files\MySQL\MySQL Workbench 8.0',
              '127.0.0.1', 'lojas', 'marcus', 'abcd.1234')
-my.fazerBackup(r'd:\bk_mysql')
+
+saida = r'c:\bk_mysql'
+my.fazerBackup(saida)
